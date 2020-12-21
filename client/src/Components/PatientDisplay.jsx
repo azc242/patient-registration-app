33187e4d5083571af1b646e6365d430889f606f9
Alan Chen <azc242@nyu.edu>
2020-12-21 10:37:58 -0800

    clean up client files/refactor

--- client/src/Components/PatientDisplay.jsx
+++ client/src/Components/PatientDisplay.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import PatientInfo from "./PatientInfo";
 
 function PatientDisplay() {
     const [patients, setPatients] = useState([]);
@@ -27,9 +26,6 @@ function PatientDisplay() {
                     <th>Time Registered</th>
                     </tr>
                 </thead>
-                {/* {patients.map((result, index) => {
-                    <div>hi</div>
-                })} */}
                 <tbody>
                 {patients.map((patient, index) => {
                     return (
@@ -45,7 +41,6 @@ function PatientDisplay() {
                 })
                 }
                 </tbody>
-                
             </table>
         </div>
     )
